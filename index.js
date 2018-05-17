1d5d2b527a7d8f650af94d1e732c3e2ae17193ae
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-17 11:18:29 -0400

    Updated: new method defintion syntax

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = {
             })
     }, 
 
-    renderListItem: function(flick) {
+    renderListItem(flick) {
         const item = document.createElement('li')
         item.textContent = flick.name
         return item
     },
 
-    handleSubmit: function(ev) {
+    handleSubmit(ev) {
         const f = ev.target
         const flick = {
             id: ++this.max,
