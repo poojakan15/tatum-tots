1f5861117bacf0603a744fea80a14aa19efd3f15
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-17 11:22:53 -0400

    Added: flicks to array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = {
-    init: function(selectors) {
+    init(selectors) {
+        this.flicks = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
         document
@@ -23,8 +24,11 @@ const app = {
             name: f.flickName.value,
         }
 
+        this.flicks.push(flick)
+
         const item = this.renderListItem(flick)
         this.list.appendChild(item)
+
         f.reset()
     },
 }
